04d67b1dcbfc246fe4f09fa1cd220df30f84d92d
dinoy <dinoykraj@gmail.com>
2024-07-25 02:27:40 +0530

    Add set up for automated maven release

--- droid-time/build.gradle.kts
+++ droid-time/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.dete.kt)
+    alias(libs.plugins.vaniktech.publish)
+    id("maven-publish")
 }
 
 android {
@@ -50,3 +54,37 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
 }
+
+mavenPublishing {
+    coordinates("io.github.dinoy-raj", "droid-time", "1.0.0")
+
+    pom {
+        name.set("Droid Time")
+        description.set("Relative and absolute date time formatting android library generates message corresponding to system default locale")
+        inceptionYear.set("2024")
+        url.set("https://github.com/dinoy-raj/droidTime")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("dinoy-raj")
+                name.set("Dinoy Raj")
+                url.set("https://github.com/dinoy-raj")
+            }
+        }
+        scm {
+            url.set("https://github.com/dinoy-raj/droidTime")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.DEFAULT, automaticRelease = true)
+    signAllPublications()
+}
+
+task("testClasses"){}
+
