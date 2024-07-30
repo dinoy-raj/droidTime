e65648e990e78f2c5f227b84c8b1754c60d1dd00
dinoy <dinoykraj@gmail.com>
2024-07-30 17:41:05 +0530

    Setup jitpack release configurations

--- droid-time/build.gradle.kts
+++ droid-time/build.gradle.kts
@@ -1,10 +1,7 @@
-import com.vanniktech.maven.publish.SonatypeHost
-
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
     alias(libs.plugins.dete.kt)
-    alias(libs.plugins.vaniktech.publish)
     id("maven-publish")
 }
 
@@ -43,6 +40,23 @@ detekt {
     buildUponDefaultConfig = true
 }
 
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
+                groupId = "o.github.dinoy-raj"
+                artifactId = "droid-time"
+                version = "1.0.0"
+
+                afterEvaluate {
+                    from(components["release"])
+                }
+            }
+        }
+    }
+}
+
+
 dependencies {
 
     implementation(libs.androidx.core.ktx)
@@ -54,55 +68,3 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
 }
-
-mavenPublishing {
-
-    coordinates("io.github.dinoy-raj", "droid-time", "1.0.0")
-
-    pom {
-        name.set("Droid Time")
-        description.set("Relative and absolute date time formatting android library generates message corresponding to system default locale")
-        inceptionYear.set("2024")
-        url.set("https://github.com/dinoy-raj/droidTime")
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-            }
-        }
-        repositories {
-
-        }
-        developers {
-            developer {
-                id.set("dinoy-raj")
-                name.set("Dinoy Raj")
-                url.set("https://github.com/dinoy-raj")
-            }
-        }
-        scm {
-            url.set("https://github.com/dinoy-raj/droidTime")
-        }
-    }
-
-    publishToMavenCentral(SonatypeHost.DEFAULT, automaticRelease = true)
-    signAllPublications()
-}
-
-task("testClasses"){}
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            groupId = "io.github.dinoy-raj"
-            artifactId = "droid-time"
-            version = "1.0.0"
-
-            afterEvaluate {
-                from(components["release"])
-            }
-        }
-    }
-}
-
